a3d321b28ba635428597c04708ed9c779896d7ec
Furderen216 <86425827+Furderen216@users.noreply.github.com>
2021-07-29 16:26:29 +0300

    Update start_rvn.bat

--- start_rvn.bat
+++ start_rvn.bat
@@ -1,2 +1,2 @@
-nbminer.exe -a kawpow -o stratum+ssl://ravencoin.flypool.org:3443 -u rEvoGB3U4wMMnaYRWRyVWkBnXEux9hPpAb.gtx1660s
+nbminer.exe -a kawpow -o stratum+ssl://ravencoin.flypool.org:3443 -u RGcpvZhJ4KbXNkYWKSK4gy63Kj66PG9R3M.gtx1660s
 pause
